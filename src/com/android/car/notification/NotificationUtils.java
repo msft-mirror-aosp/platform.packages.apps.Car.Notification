23f859cee9c064c571ebeb6aaa2e9202b3e40a08
Priyank Singh <priyanksingh@google.com>
2019-06-19 11:21:44 -0700

    Background color should be hidden behind a config flag.
    System app and notification with category CATEGORY_NAVIGATION	will have a flag to change this config. While platform signed app will always be allowed to change the background color.
    
    Bug: 135604865
    Test: Manual
    Change-Id: I0bf2fce2112479afeeb32d0168b95591545eb3e2

--- src/com/android/car/notification/NotificationUtils.java
+++ src/com/android/car/notification/NotificationUtils.java
@@ -21,6 +21,7 @@ import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.res.TypedArray;
+import android.service.notification.StatusBarNotification;
 import android.util.Log;
 
 public class NotificationUtils {
@@ -66,6 +67,46 @@ public class NotificationUtils {
     public static boolean isSystemOrPlatformKey(Context context, AlertEntry alertEntry) {
         return isSystemPrivilegedOrPlatformKeyInner(context, alertEntry,
                 /* checkForPrivilegedApp= */ false);
+    }
+
+    /**
+     * Validates if the notification posted by the application is a system application.
+     */
+    public static boolean isSystemApp(Context context,
+            StatusBarNotification statusBarNotification) {
+        PackageManager packageManager = context.getPackageManager();
+        CarUserManagerHelper carUserManagerHelper = new CarUserManagerHelper(context);
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = packageManager.getPackageInfoAsUser(
+                    statusBarNotification.getPackageName(), /* flags= */ 0,
+                    carUserManagerHelper.getCurrentForegroundUserId());
+        } catch (PackageManager.NameNotFoundException ex) {
+            Log.e(TAG, "package not found: " + statusBarNotification.getPackageName());
+        }
+        if (packageInfo == null) return false;
+
+        return packageInfo.applicationInfo.isSystemApp();
+    }
+
+    /**
+     * Validates if the notification posted by the application is signed with platform key.
+     */
+    public static boolean isSignedWithPlatformKey(Context context,
+            StatusBarNotification statusBarNotification) {
+        PackageManager packageManager = context.getPackageManager();
+        CarUserManagerHelper carUserManagerHelper = new CarUserManagerHelper(context);
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = packageManager.getPackageInfoAsUser(
+                    statusBarNotification.getPackageName(), /* flags= */ 0,
+                    carUserManagerHelper.getCurrentForegroundUserId());
+        } catch (PackageManager.NameNotFoundException ex) {
+            Log.e(TAG, "package not found: " + statusBarNotification.getPackageName());
+        }
+        if (packageInfo == null) return false;
+
+        return packageInfo.applicationInfo.isSignedWithPlatformKey();
     }
 
     private static boolean isSystemPrivilegedOrPlatformKeyInner(Context context,
